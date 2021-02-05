b27ebe9caba0db606e5355bd30018240dacfb9f7
Naman Shrimali <namanshrimali@gmail.com>
2021-02-05 07:47:38 +0530

    Added more augumentations

--- augumentations/middle_man_data_augumentation.py
+++ augumentations/middle_man_data_augumentation.py
@@ -3,6 +3,7 @@ from albumentations.pytorch import ToTensor
 
 def get_middle_man_data_aug(mean, standard_deviation):
     return [
+        A.GridDistortion (num_steps=5, distort_limit=0.3, interpolation=1, border_mode=4, value=None, mask_value=None, always_apply=False, p=0.5),
         A.ChannelDropout(channel_drop_range=(1, 1), fill_value=0, always_apply=False, p=0.5),
         A.Cutout(num_holes=1, max_h_size=16, max_w_size=16, fill_value=mean, always_apply=False, p=0.5),
         A.Normalize(mean=mean, std=standard_deviation),
