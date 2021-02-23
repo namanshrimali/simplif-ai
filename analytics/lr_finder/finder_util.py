a73a07c1f8d92a77a0a32a44d95fda3b826665db
namanshrimali <35870572+namanshrimali@users.noreply.github.com>
2021-02-23 12:54:52 +0530

    Udpated number of iterations parameter

--- analytics/lr_finder/finder_util.py
+++ analytics/lr_finder/finder_util.py
@@ -2,7 +2,7 @@ from analytics.lr_finder.lr_finder import LRFinder
 
 def find_me_ideal_lr(model, optimizer, criterion, trainloader, device, plot_graph = False, num_iter = 100, step_mode = "linear"):
     lr_finder = LRFinder(model, optimizer, criterion, device=device)
-    lr_finder.range_test(trainloader, val_loader=None, end_lr=1, num_iter=100, step_mode="linear")
+    lr_finder.range_test(trainloader, val_loader=None, end_lr=1, num_iter=num_iter, step_mode="linear")
     optimal_lr, _ = lr_finder.find_optimal_lr_loss()
     
     if plot_graph:
