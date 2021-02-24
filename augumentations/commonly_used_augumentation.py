9ef91a6c09d913af0a4c62647ec1d698e1aa1177
Naman Shrimali <namanshrimali@gmail.com>
2021-02-24 21:19:50 +0530

    Added support for more transformations

---
+++ augumentations/commonly_used_augumentation.py
@@ -0,0 +1,11 @@
+import albumentations as A
+from albumentations.pytorch import ToTensor
+
+def get_commonly_used_augumentation(mean, standard_deviation):
+    return [
+        A.Normalize(mean=mean, std=standard_deviation, always_apply=True, p=1.0),
+        A.PadIfNeeded(min_height=40, min_width=40),
+        A.RandomCrop(32, 32, always_apply=True, p=1.0),
+        A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=mean, always_apply=False, p=0.5),
+        ToTensor()
+    ]
--- data_action/transformations.py
+++ data_action/transformations.py
@@ -4,6 +4,7 @@ import torchvision.transforms as transforms
 import albumentations as A
 from augumentations.middle_man_data_augumentation import *
 from augumentations.poor_man_data_augumentation import *
+from augumentations.commonly_used_augumentation import *
 import numpy as np
 
 # requires installation of albumentations
@@ -18,5 +19,10 @@ def get_transforms(transform_type, mean, standard_deviation):
         aug = A.Compose(
             get_middle_man_data_aug(mean, standard_deviation)
             )
+        return lambda img:aug(image=np.array(img))["image"]
+    elif transform_type == 'common':
+        aug = A.Compose(
+            get_commonly_used_augumentation(mean, standard_deviation)
+        )
         return lambda img:aug(image=np.array(img))["image"]
 
