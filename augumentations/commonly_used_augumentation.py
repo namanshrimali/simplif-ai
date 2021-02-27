370d2b59e3d203df8af8a81340c50b1765d833da
Naman Shrimali <namanshrimali@gmail.com>
2021-02-27 22:39:29 +0530

    Updated augumentation cutout probability to 1

--- augumentations/commonly_used_augumentation.py
+++ augumentations/commonly_used_augumentation.py
@@ -6,6 +6,6 @@ def get_commonly_used_augumentation(mean, standard_deviation):
         A.Normalize(mean=mean, std=standard_deviation, always_apply=True, p=1.0),
         A.PadIfNeeded(min_height=40, min_width=40),
         A.RandomCrop(32, 32, always_apply=True, p=1.0),
-        A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=mean, always_apply=False, p=0.5),
+        A.Cutout(num_holes=1, max_h_size=8, max_w_size=8, fill_value=mean, always_apply=True, p=1),
         ToTensor()
     ]
