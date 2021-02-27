8130d5033a7c4617a07840b3e18c8e5cd3d003d4
Naman Shrimali <namanshrimali@gmail.com>
2021-02-27 07:15:47 +0530

    Added graph plot method

--- util/graph_plot.py
+++ util/graph_plot.py
@@ -1,8 +1,7 @@
 import matplotlib.pyplot as plt
 
 def plot_me(train_losses, train_acc, test_losses, test_acc):
-
-
+    
     fig, axs = plt.subplots(2,2,figsize=(20,15), sharex= False, sharey= False)
 
     axs[0,0].plot(train_losses)
@@ -16,3 +15,15 @@ def plot_me(train_losses, train_acc, test_losses, test_acc):
     
     axs[1, 1].plot(test_acc)
     axs[1, 1].set_title("Testing Accuracy")
+
+
+def plot_graph(data: tuple, plot_title: str, axis_lables: tuple, save: bool):
+    fig, ax = plt.subplots()
+    ax.set(xlabel=axis_lables[0], ylabel=axis_lables[1], title=plot_title)
+    ax.grid()
+    ax.plot(data[0], data[1])
+    plt.show() # dunno what it does, graphs plot without it too 
+    if save:
+        fig.savefig(f'{plot_title}.png')
+        
+    
