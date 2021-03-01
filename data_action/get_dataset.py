2be9d3e5b1db19ca01c81bb5b3a1bdfbe8d7f0fc
Naman Shrimali <namanshrimali@gmail.com>
2021-03-01 21:47:45 +0530

    Updated logic for get dataset

--- data_action/get_dataset.py
+++ data_action/get_dataset.py
@@ -1,3 +1,4 @@
+import requests
 from requests import get, HTTPError
 import zipfile
 import os
@@ -5,24 +6,39 @@ import os
 class GetDataset():
     def __init__(self, dataset):
         self.dataset = dataset
+        self.download_directory = '../data'
         
-        
-        self._download_dataset()
+        if (not os.path.isdir(self.download_directory)) or (len(os.listdir(self.download_directory)) == 0):
+            self._download_dataset()
+        else: 
+            print('Dataset already downloaded')
           
     def _download_dataset(self):
+        url = None
+        filename = None
         if self.dataset == 'TINY-IMAGENET':
-            
             url = 'http://cs231n.stanford.edu'
             filename = 'tiny-imagenet-200.zip'
             
-            try:
-                resp = requests.get(f"{url}/{filename}")
-            except HTTPError as http_error:
-                print(f'HTTP error occurred: {http_err}')
-            except Exception as exception:
-                print(f'Other error occurred: {exception}')
-            else:
-                print("Dataset doanloaded successfully")
+        try:
+            resp = requests.get(f"{url}/{filename}", allow_redirects=True, stream=True)
+        except HTTPError as http_error:
+            print(f'HTTP error occurred: {http_error}')
+        except Exception as exception:
+            print(f'Other error occurred: {exception}')
+        else:
+            print("Dataset downloaded successfully")
+            
+        filename = f"{filename}"
+        zfile = open(filename, 'wb')
+        zfile.write(resp.content)
+        zfile.close()
+
+        zipf = zipfile.ZipFile(filename, 'r')  
+        zipf.extractall(self.download_directory)
+        zipf.close()
+
+        os.remove(filename)
             
         
         
